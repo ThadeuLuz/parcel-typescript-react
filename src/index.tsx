fe2fc27acb54b33bdefe2d0e1754bc947eae83b4
Thadeu Luz <thadeuluz@gmail.com>
2018-10-24 10:27:39 -0300

    Refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 
-interface IToggleProps {
-  text: string;
-}
+import App from "./App";
 
-interface IToggleState {
-  value: boolean;
-}
-
-class Toggle extends Component<IToggleProps, IToggleState> {
-  public state = {
-    value: false
-  };
-
-  public render() {
-    const { value } = this.state;
-    const { text } = this.props;
-    return (
-      <div style={{ textAlign: "center" }}>
-        <h1>{value ? "🐱" : "🐶"}</h1>
-        <button onClick={this.toggle}>{text}</button>
-      </div>
-    );
-  }
-
-  private toggle = () => {
-    this.setState({ value: !this.state.value });
-  };
-}
-
-const App = () => <Toggle text="Click me" />;
-
-ReactDOM.render(<App />, document.getElementById("main"));
+ReactDOM.render(<App />, document.getElementById("root"));
